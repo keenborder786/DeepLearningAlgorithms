d2f3d15b170c91e3bb04bea320eaf9994e31c415
keenborder786 <21110290@lums.edu.pk>
2019-06-08 16:39:35 +0500

    Minist Dataset TensorFLow 2.0

---
+++ Minist Dataset practice.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 16:30:11 2019
+
+@author: MMOHTASHIM
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+"Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation="relu"))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+
+model.compile(loss="sparse_categorical_crossentropy",
+optimizer="sgd",
+metrics=["accuracy"])
+history = model.fit(X_train, y_train, epochs=30,
+   validation_data=(X_valid, y_valid))
+
+
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1) # set the vertical range to [0-1]
+plt.show()
+
+
+X_new=X_test[:3]
+y_proba=model.predict(X_new)
+y_proba.round(2)
+
+
+y_pred=model.predict_classes(X_new)
+
+
